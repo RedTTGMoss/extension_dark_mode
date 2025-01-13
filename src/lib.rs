1251aa0311849791acd2952637bd36f61fd63f18
JustRedTTG <redtonehair@gmail.com>
2025-01-13 17:12:11 +0200

    Update extension prefix

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,16 @@ use extism_pdk::*;
 use serde::{Deserialize, Serialize};
 
 #[plugin_fn]
-pub unsafe fn register(Json(state): Json<MossState>) -> FnResult<ExtensionInfo> {
+pub unsafe fn moss_extension_register(Json(state): Json<MossState>) -> FnResult<ExtensionInfo> {
     Ok(ExtensionInfo { files: [].to_vec() })
 }
 
 #[plugin_fn]
-pub unsafe fn extension_loop(Json(state): Json<MossState>) -> FnResult<()> {
+pub unsafe fn moss_extension_loop(Json(state): Json<MossState>) -> FnResult<()> {
     Ok(())
 }
 
 #[plugin_fn]
-pub fn unregister() -> FnResult<()> {
+pub fn moss_extension_unregister() -> FnResult<()> {
     Ok(())
 }
