f202d03decf13d02b53866161b7bc34556572676
JustRedTTG <redtonehair@gmail.com>
2025-01-25 22:14:01 +0200

    update the old code to the new definitions

--- src/lib.rs
+++ src/lib.rs
@@ -7,102 +7,41 @@ use serde::{Deserialize, Serialize};
 
 #[plugin_fn]
 pub unsafe fn moss_extension_register(Json(state): Json<MossState>) -> FnResult<ExtensionInfo> {
-    let bg_tone = 10;
-    let selected_tone = 230;
-    let line_gray = 255 - 88;
-    let line_gray_light = 255 - 167;
-    let document_gray = 255 - 184;
-    let document_background = 50;
-    let subtitle_gray = 255 - 100;
-    moss_defaults_set_color("BACKGROUND", bg_tone, bg_tone, bg_tone, 255)?;
-    moss_defaults_set_color("SELECTED", selected_tone, selected_tone, selected_tone, 255)?;
-    moss_defaults_set_color("LINE_GRAY", line_gray, line_gray, line_gray, 255)?;
-    moss_defaults_set_color(
-        "LINE_GRAY_LIGHT",
-        line_gray_light,
-        line_gray_light,
-        line_gray_light,
-        255,
-    )?;
-    moss_defaults_set_color(
-        "DOCUMENT_GRAY",
-        document_gray,
-        document_gray,
-        document_gray,
-        255,
-    )?;
-    moss_defaults_set_color(
-        "DOCUMENT_BACKGROUND",
-        document_background,
-        document_background,
-        document_background,
-        230,
-    )?;
-    moss_defaults_set_color(
-        "BUTTON_ACTIVE_COLOR",
-        selected_tone,
-        selected_tone,
-        selected_tone,
-        25,
-    )?;
-    moss_defaults_set_color(
-        "BUTTON_ACTIVE_COLOR_INVERTED",
-        bg_tone,
-        bg_tone,
-        bg_tone,
-        50,
-    )?;
-    moss_defaults_set_color(
-        "BUTTON_DISABLED_COLOR",
-        selected_tone,
-        selected_tone,
-        selected_tone,
-        100,
-    )?;
-    moss_defaults_set_color(
-        "BUTTON_DISABLED_LIGHT_COLOR",
-        bg_tone,
-        bg_tone,
-        bg_tone,
-        100,
-    )?;
-    moss_defaults_set_color(
-        "OUTLINE_COLOR",
-        selected_tone,
-        selected_tone,
-        selected_tone,
-        255,
-    )?;
-    moss_defaults_set_text_color("TEXT_COLOR", 255, 255, 255, bg_tone, bg_tone, bg_tone)?;
-    moss_defaults_set_text_color(
-        "DOCUMENT_TITLE_COLOR",
-        selected_tone,
-        selected_tone,
-        selected_tone,
-        bg_tone,
-        bg_tone,
-        bg_tone,
-    )?;
-    moss_defaults_set_text_color(
-        "DOCUMENT_TITLE_COLOR_INVERTED",
-        bg_tone,
-        bg_tone,
-        bg_tone,
-        selected_tone,
-        selected_tone,
-        selected_tone,
-    )?;
-    moss_defaults_set_text_color(
-        "DOCUMENT_SUBTITLE_COLOR",
-        subtitle_gray,
-        subtitle_gray,
-        subtitle_gray,
-        bg_tone,
-        bg_tone,
-        bg_tone,
-    )?;
-    moss_defaults_set_text_color("TEXT_COLOR_T", 255, 255, 255, -1, -1, -1)?;
-    moss_defaults_set_text_color("TEXT_COLOR_H", bg_tone, bg_tone, bg_tone, -1, -1, -1)?;
+    let white = Color::new_monochrome(255, None);
+    let background = Color::new_monochrome(30, None);
+    let selected = Color::new_monochrome(230, None);
+    let line_gray = Color::new_monochrome(255 - 88, None);
+    let line_gray_light = Color::new_monochrome(255 - 167, None);
+    let document_gray = Color::new_monochrome(255 - 184, None);
+    let document_background = Color::new_monochrome(50, Some(230));
+    let subtitle_gray = Color::new_monochrome(155, None);
+
+    let button_active_color = Color::from_existing(selected, Some(25));
+    let button_active_color_inverted = Color::from_existing(background, Some(50));
+    let button_disabled_color = Color::from_existing(selected, Some(100));
+    let button_disabled_light_color = Color::from_existing(background, Some(100));
+
+    // Base colors
+    moss_defaults_set_color("BACKGROUND".to_string(), background)?;
+    moss_defaults_set_color("SELECTED".to_string(), selected)?;
+    moss_defaults_set_color("LINE_GRAY".to_string(), line_gray)?;
+    moss_defaults_set_color("LINE_GRAY_LIGHT".to_string(), line_gray_light)?;
+    moss_defaults_set_color("DOCUMENT_GRAY".to_string(), document_gray)?;
+    moss_defaults_set_color("DOCUMENT_BACKGROUND".to_string(), document_background)?;
+    moss_defaults_set_color("BUTTON_ACTIVE_COLOR".to_string(), button_active_color)?;
+    moss_defaults_set_color("BUTTON_ACTIVE_COLOR_INVERTED".to_string(), button_active_color_inverted)?;
+    moss_defaults_set_color("BUTTON_DISABLED_COLOR".to_string(), button_disabled_color)?;
+    moss_defaults_set_color("BUTTON_DISABLED_LIGHT_COLOR".to_string(), button_disabled_light_color)?;
+    moss_defaults_set_color("OUTLINE_COLOR".to_string(), selected)?;
+
+    // Text colors
+    moss_defaults_set_text_color("TEXT_COLOR".to_string(), TextColors{ foreground: white, background: Some(background)})?;
+    moss_defaults_set_text_color("DOCUMENT_TITLE_COLOR".to_string(), TextColors{ foreground: selected, background: Some(background)})?;
+    moss_defaults_set_text_color("DOCUMENT_TITLE_COLOR_INVERTED".to_string(), TextColors { foreground: background, background: Some(selected)})?;
+    moss_defaults_set_text_color("DOCUMENT_SUBTITLE_COLOR".to_string(), TextColors { foreground: subtitle_gray, background: Some(background) })?;
+    moss_defaults_set_text_color("TEXT_COLOR_T".to_string(), TextColors { foreground: white, background: None })?;
+    moss_defaults_set_text_color("TEXT_COLOR_H".to_string(), TextColors { foreground: background, background: None })?;
+
     let mut to_invert = vec![];
     for icon in state.icons {
         if icon.ends_with("_inverted") {
