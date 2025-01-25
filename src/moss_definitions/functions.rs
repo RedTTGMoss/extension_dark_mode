0408cddc5ef49b91d7d2065c0866fca7437e9b62
JustRedTTG <redtonehair@gmail.com>
2025-01-25 20:34:51 +0200

    add monochrome color function

--- src/moss_definitions/functions.rs
+++ src/moss_definitions/functions.rs
@@ -1,62 +1,6 @@
-use crate::Color;
+use crate::moss_definitions::types::*;
 use extism_pdk::*;
 use serde::{Deserialize, Serialize};
-
-#[derive(ToBytes, Serialize, PartialEq, Debug, Clone)]
-#[encoding(Json)]
-pub struct File {
-    pub key: String,
-    pub path: String,
-}
-
-
-
-
-#[derive(ToBytes, Serialize, PartialEq, Debug)]
-#[encoding(Json)]
-pub struct ExtensionInfo {
-    pub files: Vec<File>,
-}
-
-#[derive(ToBytes, Serialize, PartialEq, Debug, Clone)]
-#[encoding(Json)]
-pub struct ContextButton {
-    pub text: String,
-    pub icon: String,
-    pub context_icon: String,
-    pub action: String,
-    pub context_menu: String,
-}
-
-#[derive(ToBytes, Serialize, PartialEq, Debug)]
-#[encoding(Json)]
-pub struct ContextMenu {
-    pub key: String,
-    pub buttons: Vec<ContextButton>,
-}
-
-#[derive(ToBytes, Deserialize, PartialEq, Debug)]
-#[encoding(Json)]
-pub struct MossState {
-    pub width: i32,
-    pub height: i32,
-    pub current_screen: String,
-    pub opened_context_menus: Vec<String>,
-    pub icons: Vec<String>,
-}
-
-#[derive(FromBytes, Deserialize, PartialEq, Debug)]
-#[encoding(Json)]
-pub struct ConfigGet<T> {
-    pub value: T,
-}
-
-#[derive(ToBytes, Serialize, PartialEq, Debug)]
-#[encoding(Json)]
-pub struct ConfigSet<T> {
-    pub key: String,
-    pub value: T,
-}
 
 #[host_fn]
 extern "ExtismHost" {
--- src/moss_definitions/types.rs
+++ src/moss_definitions/types.rs
@@ -10,6 +10,15 @@ pub struct Color {
     pub a: Option<i64>,
 }
 
+impl Color {
+    pub fn new(r: i64, g: i64, b: i64, a: Option<i64>) -> Self {
+        Color { r, g, b, a }
+    }
+    pub fn new_monochrome(color: i64, a: Option<i64>) -> Self {
+        Color { r: color, g: color, b: color, a }
+    }
+}
+
 #[derive(ToBytes, FromBytes, Deserialize, Serialize, PartialEq, Debug, Clone)]
 #[encoding(Json)]
 pub struct TextColors {
@@ -17,9 +26,55 @@ pub struct TextColors {
     pub background: Option<Color>,
 }
 
+#[derive(ToBytes, Serialize, PartialEq, Debug, Clone)]
+#[encoding(Json)]
+pub struct File {
+    pub key: String,
+    pub path: String,
+}
 
-impl Color {
-    pub fn new(r: i64, g: i64, b: i64, a: Option<i64>) -> Self {
-        Color { r, g, b, a }
-    }
+#[derive(ToBytes, Serialize, PartialEq, Debug)]
+#[encoding(Json)]
+pub struct ExtensionInfo {
+    pub files: Vec<File>,
+}
+
+#[derive(ToBytes, Serialize, PartialEq, Debug, Clone)]
+#[encoding(Json)]
+pub struct ContextButton {
+    pub text: String,
+    pub icon: String,
+    pub context_icon: String,
+    pub action: String,
+    pub context_menu: String,
+}
+
+#[derive(ToBytes, Serialize, PartialEq, Debug)]
+#[encoding(Json)]
+pub struct ContextMenu {
+    pub key: String,
+    pub buttons: Vec<ContextButton>,
+}
+
+#[derive(ToBytes, Deserialize, PartialEq, Debug)]
+#[encoding(Json)]
+pub struct MossState {
+    pub width: i32,
+    pub height: i32,
+    pub current_screen: String,
+    pub opened_context_menus: Vec<String>,
+    pub icons: Vec<String>,
+}
+
+#[derive(FromBytes, Deserialize, PartialEq, Debug)]
+#[encoding(Json)]
+pub struct ConfigGet<T> {
+    pub value: T,
+}
+
+#[derive(ToBytes, Serialize, PartialEq, Debug)]
+#[encoding(Json)]
+pub struct ConfigSet<T> {
+    pub key: String,
+    pub value: T,
 }
